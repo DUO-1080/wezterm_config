81683c6a3d7c6e8deb5199d01e1dce6951083419
Zenon <duo1080@gmail.com>
2024-02-03 21:28:59 +0800

    update font config

--- font.lua
+++ font.lua
@@ -1,12 +1,14 @@
 local wezterm = require("wezterm")
 
-local font_size = 15.6
+local font_size = 16
 local bold = false
 local font_family = ({
 	-- "JetBrainsMono NFM Medium", -- [2]
 	-- "JetBrainsMono Nerd Font", -- [2]
 	-- "FiraCode Nerd Font Mono", -- [3]
-	"JetBrains Mono",
+	-- "JetBrains Mono",
+	"Hack Nerd Font",
+	"BlexMono Nerd Font",
 })[1]
 
 local options = {}
@@ -19,4 +21,5 @@ local font = wezterm.font(font_family, options)
 return {
 	font = font,
 	font_size = font_size,
+	line_height = 1.06,
 }
