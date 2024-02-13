c3b6757a05369a7cd0293582bd37aab6bb6f461a
Zenon <duo1080@gmail.com>
2024-02-13 13:20:06 +0800

    set window decoration to "NONE"

--- window.lua
+++ window.lua
@@ -4,22 +4,23 @@ local use_background_image = false
 
 local padding = 0
 local window_padding = {
-   left = padding,
-   right = padding,
-   top = padding,
-   bottom = padding,
+	left = padding,
+	right = padding,
+	top = padding,
+	bottom = padding,
 }
 
 local M = {
-   window_padding = window_padding,
-   window_background_image_hsb = {
-      brightness = 0.3,
-   },
-   adjust_window_size_when_changing_font_size = false,
+	window_padding = window_padding,
+	window_background_image_hsb = {
+		brightness = 0.3,
+	},
+	adjust_window_size_when_changing_font_size = false,
+	window_decorations = "NONE",
 }
 
 if use_background_image then
-   M.window_background_image = path.config .. "/background.png"
+	M.window_background_image = path.config .. "/background.png"
 end
 
 return M
